a00dc894765c2e3143753e3808f3aa22c4462327
sihu <sihu03@g.skku.edu>
2024-06-05 21:34:03 +0900

    init

---
+++ ball.py
@@ -0,0 +1,15 @@
+import pygame
+from pygame.locals import *
+
+class Ball():
+    def __init__(self, screen, color, centerX=BALL_ORG_X, centerY=BALL_ORG_Y):
+            self.centerX = centerX
+                    self.centerY = centerY
+                            self.screen = screen
+                                    self.color = color
+
+                                        def drawBall(self, coord_X=BALL_ORG_X, coord_Y=BALL_ORG_Y):
+                                                pygame.draw.circle(self.screen, self.color, [coord_X, coord_Y], BALL_RADIUS)
+
+                                                    def retPos(self):
+                                                            return self.centerX, self.centerY
---
+++ brickobj.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+
+class BrickObj():
+    # Note : position of default parameter
+        def __init__(self, coordX, coordY, screen):
+                self.coordX = coordX
+                        self.coordY = coordY
+                                self.screen = screen
+
+                                    def brickDraw(self):
+                                            self.loc = [self.coordX, self.coordY]
+                                                    pygame.draw.rect(self.screen, BLUE1, [self.coordX, self.coordY, BRICK_WIDTH, BRICK_HEIGHT])
+
+                                                        def isContact(self, ball_pos):
+                                                                if (ball_pos[0] >= (self.loc[0] - BALL_RADIUS)) and (ball_pos[0] <= (self.loc[0] + BALL_RADIUS + BRICK_WIDTH)) \
+                                                                                and (ball_pos[1] >= (self.loc[1] - BALL_RADIUS)) and (
+                                                                                                ball_pos[1] <= (self.loc[1] + BALL_RADIUS + BRICK_HEIGHT)):
+                                                                                                            return True
+                                                                                                                    else:
+                                                                                                                                return False
